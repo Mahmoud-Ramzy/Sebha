496d8625143f6f805deb8b530fa4b78da0cc14d6
Mahmoud-Ramzy <anatedo4545@gmail.com>
2022-08-12 13:18:14 +0200

    first maintenance

--- script.js
+++ script.js
@@ -19,43 +19,50 @@ function Increment(){
 }
 function Clear(){Times=0;document.getElementById("root").innerText=Times;}
 function Histoory(){
-    histShowen =!histShowen;
     if(History[0]!=0){
         document.getElementById("sobhan").innerText=Name[0] + History[0];
-    }
+        document.getElementById("sobhan").style.display="block";
+    }else{document.getElementById("sobhan").style.display="none";}
     if(History[1]!=0){
         document.getElementById("elhamd").innerText=Name[1] + History[1];
-    }
+        document.getElementById("elhamd").style.display="block";
+    }else{document.getElementById("elhamd").style.display="none";}
     if(History[2]!=0){
         document.getElementById("la").innerText=Name[2] + History[2];
-    }
+        document.getElementById("la").style.display="block";
+    }else{document.getElementById("la").style.display="none";}
     if(History[3]!=0){
         document.getElementById("allah").innerText=Name[3] + History[3];
-    }
+        document.getElementById("allah").style.display="block";
+    }else{document.getElementById("allah").style.display="none";}
     if(History[4]!=0){
         document.getElementById("astaghfer").innerText=Name[4] + History[4];
-    }
+        document.getElementById("astaghfer").style.display="block";
+    }else{document.getElementById("astaghfer").style.display="none";}
     if(History[5]!=0){
         document.getElementById("sobhanWaBehamdeh").innerText=Name[5] + History[5];
-    }
+        document.getElementById("sobhanWaBehamdeh").style.display="block";
+    }else{document.getElementById("sobhanWaBehamdeh").style.display="none";}
     if(History[6]!=0){
         document.getElementById("sobhanAlazem").innerText=Name[6] + History[6];
-    }
+        document.getElementById("sobhanAlazem").style.display="block";
+    }else{document.getElementById("sobhanAlazem").style.display="none";}
     if(History[7]!=0){
         document.getElementById("profit").innerText=Name[7] + History[7];
-    }
-}
-document.getElementById("His1").addEventListener('click', ()=>{
-    if(document.getElementById("His").style.display ==='none'){
+        document.getElementById("profit").style.display="block";
+    }else{document.getElementById("profit").style.display="none";}
+//Showing the whole entity
+    if(histShowen ===false){
         document.getElementById("His1").style.marginTop ='40px';
-        // document.getElementById("His1").style.marginLeft ='150px';
         document.getElementById("His").style.display='flex';
     }
     else{document.getElementById("His").style.display ='none';
-    // document.getElementById("His1").style.marginLeft ='0px';
     document.getElementById("His1").style.marginTop ='0px';
+}//toggle the state.
+    histShowen =!histShowen;
 }
-})
+//Show history
+document.getElementById("His1").addEventListener('click', Histoory);
 
 document.getElementById("Dark").addEventListener('click', ()=>{
     document.body.style.background = '#fff';
@@ -77,7 +84,7 @@ document.getElementById("Dark").addEventListener('click', ()=>{
         document.getElementById("tasbih").style.color='rgb(167, 42, 216)';
         document.getElementById("history").style.color='#000';
     }
-})
+});
 document.getElementById("Menu").addEventListener('click', ()=>{
     if(Menu==false){
         document.getElementById("menumenu").style.display='flex';
@@ -86,4 +93,13 @@ document.getElementById("Menu").addEventListener('click', ()=>{
     else{ document.getElementById("menumenu").style.display='none';
     Menu=false;
 }
-})
+});
+
+addEventListener('resize', (event) => {
+// console.log(document.querySelector(".navbar").inn );
+if(window.innerWidth>768 ){
+    document.getElementById("menumenu").style.display='flex';
+        Menu=true;
+}
+
+});
